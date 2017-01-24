682bd6648546c8a3e440aa2765dc05f2fd8dea36
David <i22safed@uco.es>
2017-01-24 20:04:32 +0100

    Añadido el CSS

--- ElDigital/noticias/views.py
+++ ElDigital/noticias/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from .models import Post, Comentario, Mensaje
+from django.utils import timezone
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request,'noticias/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'noticias/post_list.html',{'posts':posts})
