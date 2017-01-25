e5a76f0e338b72283070426970ba790d0d235f1e
David <i22safed@uco.es>
2017-01-25 19:38:41 +0100

    Extendidas las plantillas

--- ElDigital/noticias/views.py
+++ ElDigital/noticias/views.py
@@ -10,3 +10,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'noticias/post_list.html',{'posts':posts})
+
+def post_detail(request,id_post):
+    post = get_object_or_404(Post, pk = pk)
+    return render(request, 'noticias/post_detail.html', {'post': post})
